222d8c21ea4a123fa04d4e897ae08d8b7c9dfc27
Tim Ganther <tim@yourcustomcode.com>
2020-10-09 13:40:37 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Used Themes
-1. Bootstrap Blog Theme: https://startbootstrap.com/themes/clean-blog/
+# Node.js Blog - FOM Scripting WS 2020
+## Funktionen
+Dieser Node.js Blog ist noch sehr minimal gehalten. Es wurde noch kein Usermanagement implementiert. Jeder kann einen einfachen Blogartikel erstellen und auch wieder löschen. Auf der Startseite werden alle Posts absteigend angezeigt - inklusive einer kleinen Textvorschau.
+
+## Frontend
+Dieser Blog basiert im Bezug auf das Design auf dem Bootstrap Framework und dem "Clean Blog" Template von startbootstrap.com. Es wurden ledigliche kleine Anpassungen durchgeführt und einige Seiten bzw. Elemente eingefügt.
+
+## Backend
+Serverseitig werden in diesem Projekt Node.js und Express genutzt. Zum Anzeigen dynamischer Webinhalte wird die Template Engine expressEdge verwendet. Diese gestaltet den Code durch Templates um einiges effizienter und ermöglicht das dynamische Einfügen von Inhalten in HTML Strukturen. Für das Speichern der Artikel wird eine SQLite Datenbank Datei verwendet.
+
+## Verwendete Ressourcen
+- Bootstrap: https://getbootstrap.com/
+- Bootstrap Blog Theme: https://startbootstrap.com/themes/clean-blog/
--- index.js
+++ index.js
@@ -1,16 +1,80 @@
-const express = require('express');
-
-const app = express();
-
 const path = require('path');
-app.use(express.static('assets'));
+const expressEdge = require('express-edge');
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'sites/index.html'));
+const app = new express();
+
+app.use(express.static('assets'));
+app.use(expressEdge.engine);
+app.set('views', __dirname + '/views');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+let db = new sqlite3.Database('sqlite.db', sqlite3.OPEN_READWRITE,  (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the sqlite database.');
 });
 
+app.get('/', (req, res) => {
+  let result = [];
+  db.serialize(() => {
+    db.each('SELECT * FROM posts ORDER BY id DESC', (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+      //console.log(row.id + "\t" + row.title + "\t" + row.content);
+      result.push({"id": row.id, "title": row.title, "content": row.content.substring(0, 100)+"..."});
+    });
+  });
+  res.render('index', {result})
+});
+
+app.get('/posts/neu', (req, res) => {
+    res.render('neuer-post');
+});
+
+app.post('/posts/speichern', (req, res) => {
+  db.serialize(() => {
+    db.each(`INSERT INTO posts (title, content) VALUES ("`+req.body.title+`", "`+req.body.content+`");`, (err) => {
+      if (err) {
+        console.error(err.message);
+      }
+    });
+  });
+  res.redirect('/');
+});
+
+app.post('/posts/entfernen', (req, res) => {
+  db.serialize(() => {
+    db.each('DELETE FROM posts WHERE id = '+req.body.id, (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+    });
+  });
+  res.redirect('/');
+});
+
+app.get('/posts/:id', (req, res) => {
+  let result;
+  db.serialize(() => {
+    db.each('SELECT * FROM posts WHERE id = '+req.params.id, (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+      result = {"id": row.id, "title": row.title, "content": row.content};
+      res.render('post', {result});
+    });
+  });
+});
 app.get('/hinweise', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'sites/hinweise.html'));
+    res.render('hinweise');
 });
 
 app.listen(3000, () => {
